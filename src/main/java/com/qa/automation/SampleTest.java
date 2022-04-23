a650aef5777d52aa1af1650deedea431364e8303
gem-harshit <harshit.tyagi@geminisolutions.in>
2022-04-24 01:37:06 +0530

    initial Commit

---
+++ src/main/java/com/qa/automation/SampleTest.java
@@ -0,0 +1,16 @@
+package com.qa.automation;
+
+public class SampleTest {
+
+    public static int addNumbers(int num1, int num2){
+        return (int) num1+num2;
+    }
+
+    public static int subtractNumber(int num1, int num2){
+        return (int) num1-num2;
+    }
+
+    public static int productumber(int num1, int num2){
+        return (int) num1*num2;
+    }
+}
